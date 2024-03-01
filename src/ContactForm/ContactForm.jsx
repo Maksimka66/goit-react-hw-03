a22cc760be05ba81fe3e6934be89085b7a8d27ee
Maksimka66 <maxfese@gmail.com>
2024-03-01 21:14:16 +0200

    add validation

--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -11,8 +11,23 @@ const ContactForm = ({ addUser }) => {
   const nameId = useId();
   const numberId = useId();
 
+  const userForm = Yup.object().shape({
+    username: Yup.string()
+      .min(3, "Too short!")
+      .max(50, "Too long!")
+      .required("Required!"),
+    userphone: Yup.number()
+      .min(3, "Too short!")
+      .max(50, "Too long!")
+      .required("Required!"),
+  });
+
   return (
-    <Formik initialValues={initialValues} onSubmit={addUser}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={addUser}
+      validationSchema={userForm}
+    >
       <Form>
         <label htmlFor={nameId}>Name</label>
         <Field type="text" name="username" />
