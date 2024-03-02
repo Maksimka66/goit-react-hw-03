a2b0a3d1a07db9e335367a5d3eae40df8ac86049
Maksimka66 <maxfese@gmail.com>
2024-03-02 15:08:02 +0200

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,7 +9,14 @@ import "./App.css";
 function App() {
   const [contacts, setContacts] = useState(() => {
     const contactsList = window.localStorage.getItem("saved-contacts");
-    return contactsList ? JSON.parse(contactsList) : [];
+    return contactsList
+      ? JSON.parse(contactsList)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
   });
 
   const addContact = (newContact) => {
